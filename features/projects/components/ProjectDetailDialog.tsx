613680785bcf20a7bf6dbc57838d0a9504c0e8a1
ferys2195 <fery21irawan.me@gmail.com>
2026-06-17 18:55:21 +0700

    fix: adjust dialog content class for improved responsiveness

--- features/projects/components/ProjectDetailDialog.tsx
+++ features/projects/components/ProjectDetailDialog.tsx
@@ -47,7 +47,7 @@ export function ProjectDetailDialog({
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent
-        className=" min-w-[calc(100vw-4rem)]  overflow-y-auto"
+        className="max-h-[calc(100vh-4rem)] min-w-[calc(100vw-4rem)] overflow-y-auto"
         showCloseButton
       >
         <DialogHeader>
